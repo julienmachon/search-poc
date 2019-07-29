35b54a98f59c071424147b057fa99959c5f88709
Julien Machon <julienmachon@gmail.com>
2019-07-29 10:08:57 +0200

    Add antd
    to make it all prettyyyyy

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import * as React from "react";
 import { render } from "react-dom";
+import Header from "./components/Header";
 
+import "antd/dist/antd.css";
 import "./styles.css";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <h1>Search POC</h1>
       <h2>🔍🔍🔍🔍🔍🔍</h2>
     </div>
