e2213f00ca30c1db3cf16419e05f1ce71361d926
Julien Machon <julienmachon@gmail.com>
2019-07-29 11:14:33 +0200

    Add auth (#6)
    Auth support (only works with dev)

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+export const SETTINGS = {
+  preferredRealmKey: "preferredRealm",
+  bearerTokenKey: "nexusToken",
+  serviceAccountName: "serviceaccounts",
+  clientId: "nexus-web",
+  redirectUrl: "https://o2h7l.csb.app/" // change this with whatever the codesandbox URL is
+};
