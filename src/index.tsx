f43153e07146147d6ef30e73280dd08e1cf2c8b6
Kenneth Pirman <github@kenny.wtf>
2019-07-30 17:24:12 +0200

    add basic details route (#18)

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,12 @@ import * as React from "react";
 import { render } from "react-dom";
 import { NexusClient, createNexusClient } from "@bbp/nexus-sdk";
 import { NexusProvider } from "@bbp/react-nexus";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { setUpSession, setToken } from "./utils/auth";
 import Header from "./components/Header";
 import SparqlView from "./containers/SparqlView";
 import { SETTINGS } from "./config";
+import Details from "./views/Details";
 
 import "antd/dist/antd.css";
 import "./styles.css";
@@ -24,10 +26,22 @@ async function main() {
     <NexusProvider nexusClient={nexus}>
       <div className="App">
         <Header user={user} userManager={userManager} />
-        <SparqlView
-          sparqlDatasetQueryConfig={SETTINGS.sparqlDatasetQueryConfig}
-          sparqlFilterQuery={SETTINGS.sparqlFilterQuery}
-        />
+        <Router>
+          <Switch>
+            <Route path="/" exact>
+              <SparqlView
+                sparqlDatasetQueryConfig={SETTINGS.sparqlDatasetQueryConfig}
+                sparqlFilterQuery={SETTINGS.sparqlFilterQuery}
+              />
+            </Route>
+            <Route
+              path="/details/:id"
+              render={props => {
+                return <Details id={props.match.params.id} />;
+              }}
+            />
+          </Switch>
+        </Router>
       </div>
     </NexusProvider>,
     rootElement
